cf40744fcba4c5f5d1fc8f141d40f7e4589fae75
Patrick Dornelles <vieira_patrick@live.com>
2016-10-26 16:13:07 -0200

    commit de teste

--- grammarFunctions.py
+++ grammarFunctions.py
@@ -47,21 +47,10 @@ def getGrammar(fileName):
     for var, ter in grammar.rules.items():
         for t in ter:
             temp_ter.append(''.join(t))
-        rules.append("%s -> %s" % (var, ', '.join(temp_ter)))
+        rules.append("%s -> %s" % (var, ' | '.join(temp_ter)))
         temp_ter = []
 
     result_grammar = ['Gramatica dada', 'Terminais: ' + ', '.join(grammar.terminals),
                       'Variaveis: ' + ', '.join(grammar.variables), 'Simbolo inicial: ' + ' '.join(grammar.initial_var),
                       'Regras: {' + ',\n'.join(rules) + '}']
     return '\n'.join(result_grammar)
-
-    # print "\n\n\n"
-
-    # print "Terminais: %s" % grammar.terminals
-    # print "Variaveis: %s" % grammar.variables
-    # print "Simbolo inicial: %s" % grammar.initial_var
-    # print "Regras: "
-    # for var, ter in grammar.rules.iteritems():
-    #   print " %s -> %s" % (var, ter)
-
-    # print "\n\n\n"
