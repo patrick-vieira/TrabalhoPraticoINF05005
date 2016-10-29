0790eed262e05d00f63d8b140c4ebe5e658d7eb8
Patrick Dornelles <vieira_patrick@live.com>
2016-10-29 15:43:00 -0200

    ajuste na classe

--- grammarClass.py
+++ grammarClass.py
@@ -1,10 +1,12 @@
 
 class Grammar:  # Grammar: salva variaveis, terminais e regras
-    terminals = []
-    variables = []
-    initial_var = []
-    rules = {}  # rules: um dicionario que mantem todas as regras salvas usando as variaveis como indice
-    empty_word = {}
-    useless_symbol = {}
-    excepts_empty = 0
-    has_empty = 0
+    def __init__(self):
+        self.terminals = []
+        self.variables = []
+        self.initial_var = []
+        self.rules = {}  # rules: um dicionario que mantem todas as regras salvas usando as variaveis como indice
+        self.empty_word = {}
+        self.useless_symbol = {}
+        self.excepts_empty = 0
+        self.has_empty = 0
+
